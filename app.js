aa9087a80340e34cbd38f88f2fb1fc6b9b7bd2e4
Jack Raseona <pagane.jack5@gmail.com>
2021-02-22 22:23:52 +0200

    adding MVC folder

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const appUserRoute = require('./routes/appUser');
+
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','views');
+
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(appUserRoute);
 
 app.listen(3000);
