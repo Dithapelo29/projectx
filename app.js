4b23cb1679512e42f812df6f24cf31f48a8852ba
Jack Raseona <pagane.jack5@gmail.com>
2021-03-02 23:09:35 +0200

    adding css style to the project

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const appUserRoute = require('./routes/appUser');
+const homePageRoute = require('./routes/homePage');
 
 const app = express();
 
@@ -9,7 +11,9 @@ app.set('view engine','ejs');
 app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(appUserRoute);
+app.use(homePageRoute);
 
 app.listen(3000);
--- routes/appUser.js
+++ routes/appUser.js
@@ -6,4 +6,8 @@ router.get('/',(req,res,next)=>{
     res.render('callLogPage.ejs');
 });
 
+router.post('/callLog',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
 module.exports = router;
